69773855d1300fd29e5e9b99f222b3920529d007
Artur Struzik <info@arturstruzik.pl>
2016-10-08 22:54:06 +0200

    Added partial views.

---
+++ BusinessSite/Controllers/ContactMessageController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusinessSite.Controllers
+{
+    public class ContactMessageController : Controller
+    {
+        [HttpPost]
+        public ActionResult _PartialPageContact(ContactMessage model)
+        {
+            return JavaScript("alert('Some message')");
+        }
+
+
+    }
+}
