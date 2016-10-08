99e6fc62e7d354936d26de6aeaf1ad414c9b13ca
Artur Struzik <info@arturstruzik.pl>
2016-10-08 19:31:54 +0200

    Initial commit.

---
+++ BusinessSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BusinessSite.Startup))]
+namespace BusinessSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
