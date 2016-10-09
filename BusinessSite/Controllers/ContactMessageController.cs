5eac75393c94ceaf38610639c2148ac441e6912a
Artur Struzik <info@arturstruzik.pl>
2016-10-09 09:35:49 +0200

    Added smtp client credentials.

--- BusinessSite/Controllers/ContactMessageController.cs
+++ BusinessSite/Controllers/ContactMessageController.cs
@@ -1,8 +1,7 @@
 ﻿using BusinessSite.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Mail;
 using System.Web.Mvc;
 
 namespace BusinessSite.Controllers
@@ -12,12 +11,33 @@ namespace BusinessSite.Controllers
         [HttpPost]
         public ActionResult _PartialPageContact(ContactMessage model)
         {
-            TempData["IsSuccess"] = "true";
-            TempData["ViewBag.Message"] = "Message send successfully";
+            var messageContent = "NAME: " + model.Name + " PHONE: " + model.Phone + " QUESTION: " + model.Content;
 
+            MailMessage mail = new MailMessage("info@arturstruzik.pl",
+                "info@arturstruzik.pl",
+                model.Email + " ask you a question.",
+                messageContent);
+
+            SmtpClient smtpClient = new SmtpClient("arturstruzik.nazwa.pl");
+            NetworkCredential networkCred = new NetworkCredential("info@arturstruzik.pl", "************");
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.Credentials = networkCred;
+            smtpClient.Port = 587; //port dla poczty wychodzącej
+            smtpClient.EnableSsl = false;
+            try
+            {
+                smtpClient.Send(mail);
+            }
+            catch (Exception)
+            {
+                TempData["IsSuccess"] = "false";
+                TempData["ViewBag.Message"] = "Wystąpił błąd podczas wysyłania wiadomości :(";
+                return RedirectToAction("Index", "Home");
+            }
+
+            TempData["IsSuccess"] = "true";
+            TempData["ViewBag.Message"] = "Wiadomość wysłana pomyślnie :)";
             return RedirectToAction("Index", "Home");
         }
-
-
     }
 }
